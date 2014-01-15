eae8e10a24e794c09d5615ee44c877eea0b3a908
Chris Laprun <metacosm@gmail.com>
2014-01-15 14:26:10 +0100

    Output rel so that we can more easily get it if we are iterating over a collection of links.

--- jcrestapi/src/main/java/org/jahia/modules/jcrestapi/model/JSONLink.java
+++ jcrestapi/src/main/java/org/jahia/modules/jcrestapi/model/JSONLink.java
@@ -50,6 +50,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 public class JSONLink {
+    @XmlElement
     private final String rel;
     @XmlElement(name = "href")
     private final String uri;
