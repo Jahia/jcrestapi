1f45b45ec6499f32fd32a1c518e54e210d9551ca
Thomas Draier <tdraier@jahia.com>
2021-08-24 15:38:54 +0200

    TECH-426: Adapt code to security-filter bundle (#23)

--- src/main/java/org/jahia/modules/jcrestapi/SpringBeansAccess.java
+++ src/main/java/org/jahia/modules/jcrestapi/SpringBeansAccess.java
@@ -43,7 +43,7 @@
  */
 package org.jahia.modules.jcrestapi;
 
-import org.jahia.modules.securityfilter.PermissionService;
+import org.jahia.services.securityfilter.PermissionService;
 
 import javax.jcr.Node;
 import javax.jcr.Repository;
