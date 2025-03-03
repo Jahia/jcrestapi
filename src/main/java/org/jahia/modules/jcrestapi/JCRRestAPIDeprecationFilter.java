2512c3431ef7bbb5f9f721db8f7e1854d17cbf7e
Kevan <kevan@jahia.com>
2025-03-03 10:51:42 +0100

    Better cache key, using java method name to avoid logging multiple times warning logs

--- src/main/java/org/jahia/modules/jcrestapi/JCRRestAPIDeprecationFilter.java
+++ src/main/java/org/jahia/modules/jcrestapi/JCRRestAPIDeprecationFilter.java
@@ -6,9 +6,13 @@ import org.osgi.service.cm.ConfigurationAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
@@ -21,20 +25,27 @@ import java.util.concurrent.TimeUnit;
  * A filter that logs deprecation warnings for deprecated REST API endpoints.
  */
 @Provider
+@Priority(Priorities.USER)
 public class JCRRestAPIDeprecationFilter implements ContainerResponseFilter {
 
     public static final Logger logger = LoggerFactory.getLogger(JCRRestAPIDeprecationFilter.class);
     private static final Map<String, Long> loggedPaths = new ConcurrentHashMap<>();
+
+    @Context
+    private UriInfo uriInfo;
+
+    @Context
+    private ResourceInfo resourceInfo;
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         // Always send deprecation header:
         responseContext.getHeaders().add("Deprecation", true);
 
-        // Log deprecation warning if needed:
+        // Load the config, aware that the implementation is a bit dirty and not OSGi compliant,
+        // but it's ok, remember that we are in deprecated code.
         ConfigurationAdmin configAdmin = BundleUtils.getOsgiService(ConfigurationAdmin.class, null);
         long logAgainThreshold = TimeUnit.HOURS.toMillis(24); // default re-log every 24 hours
-
         if (configAdmin != null) {
             Configuration configuration = configAdmin.getConfiguration("org.jahia.modules.jcrestapi");
             if (configuration.getProperties() != null) {
@@ -54,18 +65,17 @@ public class JCRRestAPIDeprecationFilter implements ContainerResponseFilter {
             }
         }
 
-        UriInfo uriInfo = requestContext.getUriInfo();
-        String method = requestContext.getMethod();
-        String basePath = uriInfo.getBaseUri().getPath();
-        String apiPath = uriInfo.getPath();
-        String key = method + " " + apiPath;
+        // Use java method name as cache key, useful to identify the final endpoint easily
+        String cacheKey = resourceInfo.getResourceMethod().getName();
+        Long lastLogged = loggedPaths.get(cacheKey);
         long now = Instant.now().toEpochMilli();
 
-        Long lastLogged = loggedPaths.get(key);
+        // Log the deprecation warning if it's the first time or if the threshold has been reached
         if (lastLogged == null || now - lastLogged >= logAgainThreshold) {
             logger.warn("JCR REST API is deprecated. Received a {} request to endpoint: [{}]. " +
-                    "Please refer to the GraphQL API for supported alternatives.", method, basePath + apiPath);
-            loggedPaths.put(key, now);
+                    "Please refer to the GraphQL API for supported alternatives.", requestContext.getMethod(),
+                    uriInfo.getBaseUri().getPath() + uriInfo.getPath());
+            loggedPaths.put(cacheKey, now);
         }
     }
 }
